442b8eaa6d3c34177f20dbbbb4d26266e4547f10
erschm240 <schmitze608@gmail.com>
2025-04-29 09:45:06 -0600

    Deleted Copilot config file, added ponder folder and wrote jspractice html and js files. Added portal page to root folder.

---
+++ ponder/jspractice.js
@@ -0,0 +1,36 @@
+// Variables
+const PI = 3.14;
+
+let radius = 3;
+
+let area = radius * radius * PI;
+
+console.log(area);
+
+radius = 20;
+
+area = radius * radius * PI;
+
+console.log(area);
+
+const one = 1;
+
+const two = "2";
+
+let result = one * two;
+
+console.log(result);
+
+result = one + Number(two);
+
+console.log(result);
+
+// Scope
+let globalVariable = "I'm global"
+
+function myFunction() {
+    let blockVariable = "I'm local or block level"
+    console.log(blockVariable)
+}
+
+console.log(globalVariable)
