112ebe0f2bc86279bb58dca36c3d89e1fd15702d
erschm240 <schmitze608@gmail.com>
2025-04-30 12:10:02 -0600

    Added and did startrek site target activity

---
+++ ponder/challenge/target.js
@@ -0,0 +1,5 @@
+const highlightPara = document.querySelector("#intro")
+highlightPara.style.backgroundColor = "#f2c556"
+
+const changeEm = document.querySelector("em")
+changeEm.innerText("USS Voyager Starship")
