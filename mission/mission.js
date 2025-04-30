f92add0cf404a7b4afb8d0a89d70b0fbc77ff548
erschm240 <schmitze608@gmail.com>
2025-04-30 13:17:01 -0600

    Finished dark mode mission statement assignment

--- mission/mission.js
+++ mission/mission.js
@@ -1,19 +1,21 @@
-const themeSelector = document.querySelector("select")
-themeSelector.addEventListener(changeTheme)
+let selectElem = document.querySelector("select");
+let logo = document.querySelector(".logo");
+
+selectElem.addEventListener("change", changeTheme);
 
 function changeTheme() {
-// check to see what the current value of our select is.
-// The current value is conveniently found in themeSelector.value!
-
-
-// if the value is dark then:
-// add the dark class to the body
-// change the source of the logo img to point to the white logo.
-// otherwise
-// remove the dark class
-// make sure the logo src is the blue logo.
+    // check to see what the current value of our select is.
+    // The current value is conveniently found in themeSelector.value!
+    let current = selectElem.value;
+    if (current == "dark") {
+        // add dark class to body
+        // change logo to new logo
+        document.body.className = "dark";
+        logo.src = "byui-logo_white.png";
+    } else {
+        // remove dark class
+        // change logo to original logo
+        document.body.className = "";
+        logo.src = "byui-logo_blue.webp";
+    }
 }
-
-// add an event listener to the themeSelector element here.
-// Use the changeTheme function as the event handler function.
-themeSelector.addEventListener('change', changeTheme);
