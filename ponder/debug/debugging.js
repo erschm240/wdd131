5d6cf4fce814a5b06a8edaa7eb8555705e7bb479
erschm240 <schmitze608@gmail.com>
2025-05-12 17:06:24 -0600

    Modified debugging.js file

--- ponder/debug/debugging.js
+++ ponder/debug/debugging.js
@@ -10,8 +10,8 @@ area = radius * radius * PI;
 console.log(area)
 
 function circleArea(radius) {
-    area = radius * radius * PI
-    return area
+    area = radius * radius * PI;
+    return area;
 }
 
 area = circleArea(3);
