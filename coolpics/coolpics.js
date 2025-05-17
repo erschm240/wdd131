f2844161e48a1ab1113eb2ae4cad33b851c91929
erschm240 <schmitze608@gmail.com>
2025-05-17 14:08:11 -0600

    Modifed css and js of coolpics

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,5 +1,9 @@
 const btn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
+const gallery = document.querySelector(".gallery")
+const img = document.querySelector("img")
+const modal = document.querySelector("dialog")
+const closeBtn = document.querySelector(".close-viewer")
 
 function toggleMenu() {
     menu.classList.toggle("hide");
@@ -19,3 +23,32 @@ function handleResize() {
 handleResize();
 window.addEventListener("resize", handleResize);
 
+function galleryClicked(clickEvent) {
+    clickEvent.target.closest("img");
+    const srcValue = img.src;
+    const changeImg = srcValue.split("-")[0] + "-full.jpeg";
+    showModal(changeImg);
+}
+
+function showModal(img) {
+    const modal = document.createElement('dialog');
+    modal.innerHTML = `<img src="${img}"><button class="close-viewer">X</button>`;
+    document.body.appendChild(modal);
+    modal.showModal();
+}
+
+const clickEvent = gallery.addEventListener("click", galleryClicked);
+
+function closeBtn(clickEvent) {
+    if (clickEvent.target.classList.contains("close-viewer")) {
+        modal.close();
+    }
+}
+
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+    modal.close();
+}
+});
+
+const closeModal = closeBtn.addEventListener("click");
